562c376529c5fc07bce39b16344f9790cbf7ce83
Howard Su <howard0su@gmail.com>
2025-12-25 07:57:23 +0800

    simplify the implementation of FFTRxVFO
    Avoid precreate fft plan and filters
    Avoid use table to caculate mfft and mdim

--- source_modules/sddc_source/src/fft_rx_vfo.h
+++ source_modules/sddc_source/src/fft_rx_vfo.h
@@ -41,19 +41,11 @@ namespace dsp::channel {
             _outSamplerate = 32000000;
             halfFft = fftSize / 2;
 
-            mratio[0] = 1; // 1,2,4,8,16
-            mfftdim[0] = fftSize / 2;
-            for (int i = 1; i < NDECIDX; i++) {
-                mratio[i] = mratio[i - 1] * 2;
-                mfftdim[i] = mfftdim[i - 1] / 2;
-            }
 
             _mtunebin = fftSize / 2 / 4;
+            GainScale = gain;
 
-            for (int d = 0; d < NDECIDX; d++) {
-                filterHw[d] = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * halfFft); // halfFft
-            }
-
+            filter = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * halfFft);                  // halfFft
             ADCinTime = (float*)fftwf_malloc(1024 * 1024 * sizeof(float) + sizeof(float) * halfFft); // large enough buffer
             // make sure initial overlap region is zeroed to avoid reading uninitialized samples
             memset(ADCinTime, 0, sizeof(float) * halfFft);
@@ -61,25 +53,28 @@ namespace dsp::channel {
             ADCinFreq = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * (halfFft + 1)); // 1024+1
             inFreqTmp = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * (halfFft));     // 1024
 
-            plan_t2f_r2c = fftwf_plan_dft_r2c_1d(2 * halfFft, ADCinTime, ADCinFreq, FFTW_MEASURE);
-            for (int d = 0; d < NDECIDX; d++) {
-                plans_f2t_c2c[d] = fftwf_plan_dft_1d(mfftdim[d], inFreqTmp, inFreqTmp, FFTW_BACKWARD, FFTW_MEASURE);
-            }
+            plan_t2f_r2c = fftwf_plan_dft_r2c_1d(2 * halfFft, ADCinTime, ADCinFreq, FFTW_PATIENT);
+            plan_f2t_c2c = fftwf_plan_dft_1d(halfFft, inFreqTmp, inFreqTmp, FFTW_BACKWARD, FFTW_PATIENT);
 
-            // create each filters
-            generateFreqFilters(1.0f);
-
+            generateFreqFilter(gain, 0);
             base_type::init(in);
         }
 
         void setGainFactor(float gain) {
             std::lock_guard<std::recursive_mutex> lck(base_type::ctrlMtx);
-            generateFreqFilters(gain);
+            if (GainScale != gain) {
+                generateFreqFilter(gain, _decimationIndex);
+            }
         }
 
         void setInSamplerate(double inSamplerate) {
             assert(base_type::_block_init);
             std::lock_guard<std::recursive_mutex> lck(base_type::ctrlMtx);
+
+            if (_inSamplerate == inSamplerate) {
+                return;
+            }
+
             base_type::tempStop();
 
             _inSamplerate = inSamplerate;
@@ -90,14 +85,21 @@ namespace dsp::channel {
             }
 
             _decimationIndex -= 1; // index starts from zero
+
+            // create each filters
+            generateFreqFilter(GainScale, _decimationIndex);
+
             base_type::tempStart();
         }
 
         void setOutSamplerate(double outSamplerate, double bandwidth) {
             assert(base_type::_block_init);
             std::lock_guard<std::recursive_mutex> lck(base_type::ctrlMtx);
-            base_type::tempStop();
 
+            if (_outSamplerate == outSamplerate) {
+                return;
+            }
+            base_type::tempStop();
             _outSamplerate = outSamplerate;
             _decimationIndex = (int)(log2(_inSamplerate / _outSamplerate));
             if (_decimationIndex < 1) {
@@ -106,6 +108,7 @@ namespace dsp::channel {
             }
 
             _decimationIndex -= 1; // index starts from zero
+            generateFreqFilter(GainScale, _decimationIndex);
             base_type::tempStart();
         }
 
@@ -123,7 +126,7 @@ namespace dsp::channel {
             _mtunebin = int(offset * halfFft / 4) * 4; // mtunebin step 4 bin  ?
             // handle the small freq drift
             float delta = ((float)_mtunebin / halfFft) - offset;
-            float fc = delta * mratio[_decimationIndex]; // ret increases with higher decimation
+            float fc = delta * (1 << _decimationIndex); // ret increases with higher decimation
             // DbgPrintf("offset %f mtunebin %d delta %f (%f)\n", offset, this->mtunebin, delta, ret);
             if (_lsb) fc = -fc;
 
@@ -143,19 +146,17 @@ namespace dsp::channel {
         inline int process(int count, const int16_t* in, complex_t* out) {
             int decimate;
             int mtunebin;
+            bool lsb;
 
             {
                 std::lock_guard<std::recursive_mutex> lck(base_type::ctrlMtx);
                 decimate = _decimationIndex;
                 mtunebin = _mtunebin;
+                lsb = _lsb;
             }
 
-            fftwf_plan* plan_f2t_c2c; // fftw plan buffers Time to Freq real to complex per buffer
-            plan_f2t_c2c = &plans_f2t_c2c[decimate];
-
             // holds the FFT size for the current decimation level
-            int mfft = this->mfftdim[decimate]; // = halfFft / 2^mdecimation
-            fftwf_complex* filter = filterHw[decimate];
+            int mfft = halfFft / (1 << decimate); // = halfFft / 2^mdecimation
 
             // when arriving here, we have 'count' new samples in 'in'
             // and we have halffft samples overlapped from last time in ADCInTime
@@ -178,26 +179,26 @@ namespace dsp::channel {
             // calcuate how many times we should run fft/ifft pair
             // we can estimate how many output samples we can get
             int fftPerBuf = count / (3 * halfFft / 2) + 1; // number of ffts per buffer with 256|768 overlap
-
-            for (int k = 0; k < fftPerBuf; k++) {
-                // core of fast convolution including filter and decimation
-                //   main part is 'overlap-scrap' (IMHO better name for 'overlap-save'), see
-                //   https://en.wikipedia.org/wiki/Overlap%E2%80%93save_method
+            float* real_input = ADCinTime;
+            {
+                // k == 0, first case
                 {
                     // FFT first stage: time to frequency, real to complex
                     // 'full' transformation size: 2 * halfFft
-                    fftwf_execute_dft_r2c(plan_t2f_r2c, ADCinTime + (3 * halfFft / 2) * k, ADCinFreq);
+                    fftwf_execute_dft_r2c(plan_t2f_r2c, real_input, ADCinFreq);
+                    real_input += 3 * halfFft / 2;
                     // result now in ADCinFreq[]
-
+                    ADCinFreq[0][0] = 0;
+                    ADCinFreq[0][1] = 0;
                     // circular shift (mixing in full bins) and low/bandpass filtering (complex multiplication)
                     {
                         // circular shift tune fs/2 first half array into inFreqTmp[]
-                        shift_freq(inFreqTmp, source, filter, 0, shift_count);
+                        shift_freq(inFreqTmp, source, filter, shift_count);
                         if (mfft / 2 != shift_count)
                             memset(inFreqTmp[shift_count], 0, sizeof(*inFreqTmp) * (mfft / 2 - shift_count));
 
                         // circular shift tune fs/2 second half array
-                        shift_freq(dest, source2, filter2, start, mfft / 2);
+                        shift_freq(&dest[start], &source2[start], &filter2[start], mfft / 2 - start);
                         if (start != 0)
                             memset(inFreqTmp[mfft / 2], 0, sizeof(*inFreqTmp) * start);
                     }
@@ -205,34 +206,68 @@ namespace dsp::channel {
 
                     // 'shorter' inverse FFT transform (decimation); frequency (back) to COMPLEX time domain
                     // transform size: mfft = mfftdim[k] = halfFft / 2^k with k = mdecimation
-                    fftwf_execute_dft(*plan_f2t_c2c, inFreqTmp, inFreqTmp); //  c2c decimation
+                    fftwf_execute_dft(plan_f2t_c2c, inFreqTmp, inFreqTmp); //  c2c decimation
                     // result now in inFreqTmp[]
                 }
 
                 // postprocessing
-                if (_lsb) // lower sideband
+                if (lsb) // lower sideband
                 {
                     // mirror just by negating the imaginary Q of complex I/Q
-                    if (k == 0) {
-                        copy<true>((fftwf_complex*)out, &inFreqTmp[mfft / 4], mfft / 2);
-                        out += mfft / 2;
-                    }
-                    else {
-                        copy<true>((fftwf_complex*)out, inFreqTmp, (3 * mfft / 4));
-                        out += 3 * mfft / 4;
-                    }
+                    copy<true>((fftwf_complex*)out, &inFreqTmp[mfft / 4], mfft / 2);
                 }
                 else // upper sideband
                 {
-                    if (k == 0) {
-                        copy<false>((fftwf_complex*)out, &inFreqTmp[mfft / 4], mfft / 2);
-                        out += mfft / 2;
+                    copy<false>((fftwf_complex*)out, &inFreqTmp[mfft / 4], mfft / 2);
+                }
+                out += mfft / 2;
+            }
+
+            int output_step = 3 * mfft / 4;
+            for (int k = 1; k < fftPerBuf; k++) {
+                // core of fast convolution including filter and decimation
+                //   main part is 'overlap-scrap' (IMHO better name for 'overlap-save'), see
+                //   https://en.wikipedia.org/wiki/Overlap%E2%80%93save_method
+                // FFT first stage: time to frequency, real to complex
+                // 'full' transformation size: 2 * halfFft
+                fftwf_execute_dft_r2c(plan_t2f_r2c, real_input, ADCinFreq);
+                real_input += 3 * halfFft / 2;
+                // result now in ADCinFreq[]
+                ADCinFreq[0][0] = 0;
+                ADCinFreq[0][1] = 0;
+
+                // circular shift (mixing in full bins) and low/bandpass filtering (complex multiplication)
+                {
+                    // circular shift tune fs/2 first half array into inFreqTmp[]
+                    shift_freq(inFreqTmp, source, filter, shift_count);
+                    if (mfft / 2 != shift_count)
+                        memset(inFreqTmp[shift_count], 0, sizeof(*inFreqTmp) * (mfft / 2 - shift_count));
+
+                    // circular shift tune fs/2 second half array
+                    shift_freq(&dest[start], &source2[start], &filter2[start], mfft / 2 - start);
+                    if (start != 0)
+                        memset(inFreqTmp[mfft / 2], 0, sizeof(*inFreqTmp) * start);
+                }
+                // result now in inFreqTmp[]
+
+                fftwf_execute_dft(plan_f2t_c2c, inFreqTmp, (fftwf_complex*)out); //  c2c decimation
+
+                // postprocessing
+                if (lsb) // lower sideband
+                {
+                    // mirror just by negating the imaginary Q of complex I/Q
+                    int i;
+                    for (i = 0; i < output_step; i += 4) {
+                        out[i].im = -out[i].im;
+                        out[i + 1].im = -out[i + 1].im;
+                        out[i + 2].im = -out[i + 2].im;
+                        out[i + 3].im = -out[i + 3].im;
                     }
-                    else {
-                        copy<false>((fftwf_complex*)out, inFreqTmp, (3 * mfft / 4));
-                        out += 3 * mfft / 4;
+                    for (; i < output_step; i++) {
+                        out[i].im = -out[i].im;
                     }
                 }
+                out += output_step;
                 // result now in this->obuffers[]
             }
 
@@ -266,9 +301,9 @@ namespace dsp::channel {
             volk_16i_s32f_convert_32f(output, input, 1.0f, count);
         }
 
-        static inline void shift_freq(fftwf_complex* dest, const fftwf_complex* source1, const fftwf_complex* source2, int start, int end) {
+        static inline void shift_freq(fftwf_complex* dest, const fftwf_complex* source1, const fftwf_complex* source2, int count) {
             // Use VOLK for complex multiplication
-            volk_32fc_x2_multiply_32fc((lv_32fc_t*)(dest + start), (lv_32fc_t*)(source1 + start), (lv_32fc_t*)(source2 + start), end - start);
+            volk_32fc_x2_multiply_32fc((lv_32fc_t*)(dest), (lv_32fc_t*)(source1), (lv_32fc_t*)(source2), count);
         }
 
         template <bool flip>
@@ -285,23 +320,22 @@ namespace dsp::channel {
             }
         }
 
-        void generateFreqFilters(float gain) {
+        void generateFreqFilter(float gain, int index) {
             fftwf_plan filterplan_t2f_c2c; // time to frequency fft
-
-            // filters
-            fftwf_complex* pfilterht; // time filter ht
+            fftwf_complex* pfilterht;      // time filter ht
 
             pfilterht = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * halfFft); // halfFft
-            filterplan_t2f_c2c = fftwf_plan_dft_1d(halfFft, pfilterht, filterHw[0], FFTW_FORWARD, FFTW_MEASURE);
+            filterplan_t2f_c2c = fftwf_plan_dft_1d(halfFft, pfilterht, filter, FFTW_FORWARD, FFTW_ESTIMATE);
+
             float* pht = new float[halfFft / 4 + 1];
             const float Astop = 120.0f;
-            const float relPass = 0.85f;      // 85% of Nyquist should be usable
-            const float relStop = 1.1f;       // 'some' alias back into transition band is OK
-            for (int d = 0; d < NDECIDX; d++) // @todo when increasing NDECIDX
+            const float relPass = 0.85f; // 85% of Nyquist should be usable
+            const float relStop = 1.1f;  // 'some' alias back into transition band is OK
+
             {
                 // @todo: have dynamic bandpass filter size - depending on decimation
                 //   to allow same stopband-attenuation for all decimations
-                float Bw = 64.0f / mratio[d];
+                float Bw = 64.0f / (1 << index); // bandwidth relative to fs=64MHz
                 // Bw *= 0.8f;  // easily visualize Kaiser filter's response
                 KaiserWindow(halfFft / 4 + 1, Astop, relPass * Bw / 128.0f, relStop * Bw / 128.0f, pht);
 
@@ -315,11 +349,14 @@ namespace dsp::channel {
                     pfilterht[halfFft - 1 - t][0] = gainadj * pht[t];
                 }
 
-                fftwf_execute_dft(filterplan_t2f_c2c, pfilterht, filterHw[d]);
+                fftwf_execute_dft(filterplan_t2f_c2c, pfilterht, filter);
             }
             delete[] pht;
             fftwf_destroy_plan(filterplan_t2f_c2c);
             fftwf_free(pfilterht);
+
+            fftwf_destroy_plan(plan_f2t_c2c);
+            plan_f2t_c2c = fftwf_plan_dft_1d(halfFft / (1 << _decimationIndex), inFreqTmp, inFreqTmp, FFTW_BACKWARD, FFTW_PATIENT);
         }
 
         void cleanup() {
@@ -327,14 +364,13 @@ namespace dsp::channel {
                 return;
             }
 
-            for (int d = 0; d < NDECIDX; d++) {
-                fftwf_free(filterHw[d]); // 4096
+            if (filter != nullptr) {
+                fftwf_free(filter);
+                filter = nullptr;
             }
 
             fftwf_destroy_plan(plan_t2f_r2c);
-            for (int d = 0; d < NDECIDX; d++) {
-                fftwf_destroy_plan(plans_f2t_c2c[d]);
-            }
+            fftwf_destroy_plan(plan_f2t_c2c);
 
             fftwf_free(ADCinTime);
             fftwf_free(ADCinFreq);
@@ -343,12 +379,9 @@ namespace dsp::channel {
         }
 
     private:
-        int mratio[NDECIDX];  // ratio dimentions: mratio[k] = 2^k
-        int mfftdim[NDECIDX]; // FFT N dimensions: mfftdim[k] = halfFft / 2^k
-
-        fftwf_complex* filterHw[NDECIDX]; // Hw complex to each decimation ratio
-        fftwf_plan plans_f2t_c2c[NDECIDX];
-
+        // fftwf_complex* filterHw[NDECIDX]; // Hw complex to each decimation ratio
+        fftwf_complex* filter = nullptr;
+        fftwf_plan plan_f2t_c2c;
         fftwf_plan plan_t2f_r2c; // fftw plan buffers Freq to Time complex to complex per decimation ratio
 
         float* ADCinTime;         // point to each threads input buffers [nftt]
